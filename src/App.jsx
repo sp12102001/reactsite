0afc4fd5ea2dc0426a7ab2e31b3d2d087331951c
SP <129768978+sp12102001@users.noreply.github.com>
2024-06-19 20:52:34 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -181,12 +181,12 @@ function App() {
           </div>
         </section>
         <projects />
-        <section id="service-iframe" className="container">
+<section id="service-iframe" className="container">
   <h2>My Projects</h2>
-  <div style={{ position: 'relative', width: '100%', height: 0, paddingTop: '56.25%', paddingBottom: 0, boxShadow: '0 2px 8px 0 rgba(63,69,81,0.16)', marginTop: '1.6em', marginBottom: '0.9em', overflow: 'hidden', borderRadius: '8px', willChange: 'transform' }}>
+  <div style={{ position: 'relative', width: '100%', height: 0, paddingTop: '56.25%', paddingBottom: 0, boxShadow: '0 2px 8px 0 rgba(63,69,81,0.16)', marginTop: '1.6em', marginBottom: '0.9em', overflow: 'hidden', borderRadius: '8px', willChange: 'transform', transform: 'scale(0.85)', transformOrigin: '0 0' }}>
     <iframe 
       loading="lazy" 
-      style={{ position: 'absolute', width: '100%', height: '100%', top: 0, left: 0, border: 'none', padding: 0, margin: 0 }} 
+      style={{ position: 'absolute', width: '118%', height: '118%', top: 0, left: 0, border: 'none', padding: 0, margin: 0 }} 
       src="https://sp12102001.github.io/my-projects/" 
       allowFullScreen="allowfullscreen" 
       allow="fullscreen"
