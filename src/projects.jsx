b6df0fa7de7bb207b6a0acd946468c29f3be7c8a
SP <129768978+sp12102001@users.noreply.github.com>
2024-06-20 08:34:35 +0100

    Update projects.jsx

--- src/projects.jsx
+++ src/projects.jsx
@@ -1,159 +1,84 @@
 import React from 'react';
 import './projects.css'; // Assuming you have a CSS file for Projects-specific styles
 
-function projects() {
+function Projects() {
+  const projectData = [
+    {
+      name: 'Systematic-Synthesizer',
+      description: 'Automates the systematic review process using NLP, ML, and data extraction techniques.',
+      repoUrl: 'https://github.com/sp12102001/Systematic-Synthesizer',
+      imageUrl: 'https://github.com/sp12102001/Systematic-Synthesizer/raw/main/preview.png',
+      builtWith: 'JavaScript, HTML, CSS, jQuery, Bootstrap, Node'
+    },
+    {
+      name: 'Tax-Questions-Bot',
+      description: 'A web-based tool to help individuals in Singapore with tax-related queries.',
+      repoUrl: 'https://github.com/sp12102001/Tax-Questions-Bot',
+      imageUrl: 'https://github.com/sp12102001/Tax-Questions-Bot/raw/main/preview.png',
+      builtWith: 'JavaScript'
+    },
+    {
+      name: 'API Query Framework',
+      description: 'Framework for creating a static website that makes API queries using JavaScript and integrates ChatGPT.',
+      repoUrl: 'https://github.com/sp12102001/api-query-framework',
+      imageUrl: 'https://github.com/sp12102001/api-query-framework/raw/main/preview.png',
+      builtWith: 'JavaScript, HTML, CSS'
+    },
+    {
+      name: 'Network Me',
+      description: 'Starting framework for an auth-included social media application.',
+      repoUrl: 'https://github.com/sp12102001/network-me',
+      imageUrl: 'https://github.com/sp12102001/network-me/raw/main/preview.png',
+      builtWith: 'JavaScript, HTML, CSS, jQuery, Bootstrap, Node'
+    },
+    {
+      name: 'Research Agent AI',
+      description: 'Autonomously conducts research to write systematic review structured scientific papers.',
+      repoUrl: 'https://github.com/sp12102001/research-agent-ai',
+      imageUrl: 'https://github.com/sp12102001/research-agent-ai/raw/main/preview.png',
+      builtWith: 'JavaScript, HTML, CSS, jQuery, Bootstrap, Node'
+    },
+    {
+      name: 'AI-Powered Email Marketing Platform',
+      description: 'AI optimizes email campaigns with features like subject line generation and content personalization.',
+      repoUrl: 'https://github.com/sp12102001/ai-email-marketing',
+      imageUrl: 'https://github.com/sp12102001/ai-email-marketing/raw/main/preview.png',
+      builtWith: 'JavaScript, HTML, CSS, jQuery, Bootstrap, Node'
+    },
+    {
+      name: 'Python Game',
+      description: 'A Python-based interactive maths game where the human plays against the computer.',
+      repoUrl: 'https://github.com/sp12102001/python-game',
+      imageUrl: 'https://github.com/sp12102001/python-game/raw/main/preview.png',
+      builtWith: 'Python'
+    },
+    {
+      name: 'Apology-Support-AI',
+      description: 'A satirical initiative generating personalized gift ideas based on parameters like the cause of an argument, age, budget, and severity.',
+      repoUrl: 'https://github.com/sp12102001/Apology-Support-AI',
+      imageUrl: 'https://github.com/sp12102001/Apology-Support-AI/raw/main/preview.png',
+      builtWith: 'JavaScript, HTML, CSS, Node'
+    }
+  ];
+
   return (
     <section id="projects" className="container">
       <h2>My Projects</h2>
-      <ol className="d-flex flex-wrap list-style-none gutter-condensed mb-2 js-pinned-items-reorder-list">
-        
-        <li className="mb-3 d-flex flex-content-stretch sortable-button-item pinned-item-list-item js-pinned-item-list-item col-12 col-md-6 col-lg-6 reorderable">
-          <div className="Box d-flex p-3 width-full public source card">
-            <div className="pinned-item-list-item-content">
-              <div className="d-flex width-full position-relative">
-                <div className="flex-1">
-                  <svg aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true" className="octicon octicon-repo mr-1 color-fg-muted">
-                    <path d="M2 2.5A2.5 2.5 0 0 1 4.5 0h8.75a.75.75 0 0 1 .75.75v12.5a.75.75 0 0 1-.75.75h-2.5a.75.75 0 0 1 0-1.5h1.75v-2h-8a1 1 0 0 0-.714 1.7.75.75 0 1 1-1.072 1.05A2.495 2.495 0 0 1 2 11.5Zm10.5-1h-8a1 1 0 0 0-1 1v6.708A2.486 2.486 0 0 1 4.5 9h8ZM5 12.25a.25.25 0 0 1 .25-.25h3.5a.25.25 0 0 1 .25.25v3.25a.25.25 0 0 1-.4.2l-1.45-1.087a.249.249 0 0 0-.3 0L5.4 15.7a.25.25 0 0 1-.4-.2Z"></path>
-                  </svg>
-                  <span data-view-component="true" className="position-relative">
-                    <a data-hydro-click='{"event_type":"user_profile.click","payload":{"profile_user_id":129768978,"target":"PINNED_REPO","user_id":129768978,"originating_url":"https://github.com/sp12102001"}}' data-hydro-click-hmac="87ab20961ec4dad289769a689e3b29bad16e4d0611960311e38d90fca6aa8863" id="766327042" href="/sp12102001/Systematic-Synthesizer" data-view-component="true" className="Link mr-1 text-bold wb-break-word" aria-labelledby="tooltip-5f64eef8-1444-4024-acab-2399f0d18edf">
-                      <span className="repo">Systematic-Synthesizer</span>
-                    </a>
-                    <tool-tip id="tooltip-5f64eef8-1444-4024-acab-2399f0d18edf" for="766327042" popover="manual" data-direction="s" data-type="label" data-view-component="true" className="sr-only position-absolute" aria-hidden="true" role="tooltip">Systematic-Synthesizer</tool-tip>
-                  </span>
-                  <span></span><span className="Label Label--secondary v-align-middle mt-1 no-wrap v-align-baseline Label--inline">Public</span>
-                </div>
-              </div>
-              <p className="pinned-item-desc color-fg-muted text-small mt-2 mb-0">
-                The application aims to automate the systematic review process, leveraging natural language processing (NLP), machine learning (ML), and data extraction techniques to streamline research design, li…
-              </p>
+      <div className="projects-grid">
+        {projectData.map((project, index) => (
+          <div key={index} className="card">
+            <img src={project.imageUrl} alt={`${project.name} preview`} className="project-image" />
+            <div className="card-content">
+              <h3>{project.name}</h3>
+              <p className="description">{project.description}</p>
+              <p className="built-with"><strong>Built With:</strong> {project.builtWith}</p>
+              <a href={project.repoUrl} className="btn" target="_blank" rel="noopener noreferrer">View Project on GitHub</a>
             </div>
           </div>
-        </li>
-        
-        <li className="mb-3 d-flex flex-content-stretch sortable-button-item pinned-item-list-item js-pinned-item-list-item col-12 col-md-6 col-lg-6 reorderable">
-          <div className="Box d-flex p-3 width-full public source card">
-            <div className="pinned-item-list-item-content">
-              <div className="d-flex width-full position-relative">
-                <div className="flex-1">
-                  <svg aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true" className="octicon octicon-repo mr-1 color-fg-muted">
-                    <path d="M2 2.5A2.5 2.5 0 0 1 4.5 0h8.75a.75.75 0 0 1 .75.75v12.5a.75.75 0 0 1-.75.75h-2.5a.75.75 0 0 1 0-1.5h1.75v-2h-8a1 1 0 0 0-.714 1.7.75.75 0 1 1-1.072 1.05A2.495 2.495 0 0 1 2 11.5Zm10.5-1h-8a1 1 0 0 0-1 1v6.708A2.486 2.486 0 0 1 4.5 9h8ZM5 12.25a.25.25 0 0 1 .25-.25h3.5a.25.25 0 0 1 .25.25v3.25a.25.25 0 0 1-.4.2l-1.45-1.087a.249.249 0 0 0-.3 0L5.4 15.7a.25.25 0 0 1-.4-.2Z"></path>
-                  </svg>
-                  <span data-view-component="true" className="position-relative">
-                    <a data-hydro-click='{"event_type":"user_profile.click","payload":{"profile_user_id":129768978,"target":"PINNED_REPO","user_id":129768978,"originating_url":"https://github.com/sp12102001"}}' data-hydro-click-hmac="87ab20961ec4dad289769a689e3b29bad16e4d0611960311e38d90fca6aa8863" id="768292900" href="/sp12102001/Tax-Questions-Bot" data-view-component="true" className="Link mr-1 text-bold wb-break-word" aria-labelledby="tooltip-a020cae5-4d85-42af-98cc-307734e02ada">
-                      <span className="repo">Tax-Questions-Bot</span>
-                    </a>
-                    <tool-tip id="tooltip-a020cae5-4d85-42af-98cc-307734e02ada" for="768292900" popover="manual" data-direction="s" data-type="label" data-view-component="true" className="sr-only position-absolute" aria-hidden="true" role="tooltip">Tax-Questions-Bot</tool-tip>
-                  </span>
-                  <span></span><span className="Label Label--secondary v-align-middle mt-1 no-wrap v-align-baseline Label--inline">Public</span>
-                </div>
-              </div>
-              <p className="pinned-item-desc color-fg-muted text-small mt-2 mb-0">
-                The Tax Questions Bot is a web-based tool designed to help individuals determine their tax-related queries, specifically in Singapore. This bot provides answers to common questions related to incom…
-              </p>
-            </div>
-          </div>
-        </li>
-        
-        <li className="mb-3 d-flex flex-content-stretch sortable-button-item pinned-item-list-item js-pinned-item-list-item col-12 col-md-6 col-lg-6 reorderable">
-          <div className="Box d-flex p-3 width-full public source card">
-            <div className="pinned-item-list-item-content">
-              <div className="d-flex width-full position-relative">
-                <div className="flex-1">
-                  <svg aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true" className="octicon octicon-repo mr-1 color-fg-muted">
-                    <path d="M2 2.5A2.5 2.5 0 0 1 4.5 0h8.75a.75.75 0 0 1 .75.75v12.5a.75.75 0 0 1-.75.75h-2.5a.75.75 0 0 1 0-1.5h1.75v-2h-8a1 1 0 0 0-.714 1.7.75.75 0 1 1-1.072 1.05A2.495 2.495 0 0 1 2 11.5Zm10.5-1h-8a1 1 0 0 0-1 1v6.708A2.486 2.486 0 0 1 4.5 9h8ZM5 12.25a.25.25 0 0 1 .25-.25h3.5a.25.25 0 0 1 .25.25v3.25a.25.25 0 0 1-.4.2l-1.45-1.087a.249.249 0 0 0-.3 0L5.4 15.7a.25.25 0 0 1-.4-.2Z"></path>
-                  </svg>
-                  <span data-view-component="true" className="position-relative">
-                    <a data-hydro-click='{"event_type":"user_profile.click","payload":{"profile_user_id":129768978,"target":"PINNED_REPO","user_id":129768978,"originating_url":"https://github.com/sp12102001"}}' data-hydro-click-hmac="87ab20961ec4dad289769a689e3b29bad16e4d0611960311e38d90fca6aa8863" id="738867386" href="/sp12102001/Apology-Support-AI" data-view-component="true" className="Link mr-1 text-bold wb-break-word" aria-labelledby="tooltip-8da69f5f-b922-42fb-a5a2-f3d161e95df3">
-                      <span className="repo">Apology-Support-AI</span>
-                    </a>
-                    <tool-tip id="tooltip-8da69f5f-b922-42fb-a5a2-f3d161e95df3" for="738867386" popover="manual" data-direction="s" data-type="label" data-view-component="true" className="sr-only position-absolute" aria-hidden="true" role="tooltip">Apology-Support-AI</tool-tip>
-                  </span>
-                  <span></span><span className="Label Label--secondary v-align-middle mt-1 no-wrap v-align-baseline Label--inline">Public</span>
-                </div>
-              </div>
-              <p className="pinned-item-desc color-fg-muted text-small mt-2 mb-0">
-                A satarical initiative leveraging Cloudflare Workers to dynamically generate personalized gift ideas based on parameters like the cause of an argument, age, budget, and severity.
-              </p>
-            </div>
-          </div>
-        </li>
-        
-        <li className="mb-3 d-flex flex-content-stretch sortable-button-item pinned-item-list-item js-pinned-item-list-item col-12 col-md-6 col-lg-6 reorderable">
-          <div className="Box d-flex p-3 width-full public source card">
-            <div className="pinned-item-list-item-content">
-              <div className="d-flex width-full position-relative">
-                <div className="flex-1">
-                  <svg aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true" className="octicon octicon-repo mr-1 color-fg-muted">
-                    <path d="M2 2.5A2.5 2.5 0 0 1 4.5 0h8.75a.75.75 0 0 1 .75.75v12.5a.75.75 0 0 1-.75.75h-2.5a.75.75 0 0 1 0-1.5h1.75v-2h-8a1 1 0 0 0-.714 1.7.75.75 0 1 1-1.072 1.05A2.495 2.495 0 0 1 2 11.5Zm10.5-1h-8a1 1 0 0 0-1 1v6.708A2.486 2.486 0 0 1 4.5 9h8ZM5 12.25a.25.25 0 0 1 .25-.25h3.5a.25.25 0 0 1 .25.25v3.25a.25.25 0 0 1-.4.2l-1.45-1.087a.249.249 0 0 0-.3 0L5.4 15.7a.25.25 0 0 1-.4-.2Z"></path>
-                  </svg>
-                  <span data-view-component="true" className="position-relative">
-                    <a data-hydro-click='{"event_type":"user_profile.click","payload":{"profile_user_id":129768978,"target":"PINNED_REPO","user_id":129768978,"originating_url":"https://github.com/sp12102001"}}' data-hydro-click-hmac="87ab20961ec4dad289769a689e3b29bad16e4d0611960311e38d90fca6aa8863" id="794058192" href="/sp12102001/network-me" data-view-component="true" className="Link mr-1 text-bold wb-break-word" aria-labelledby="tooltip-18fa0e3f-06f3-4770-b86f-8f9fb233e7d9">
-                      <span className="repo">network-me</span>
-                    </a>
-                    <tool-tip id="tooltip-18fa0e3f-06f3-4770-b86f-8f9fb233e7d9" for="794058192" popover="manual" data-direction="s" data-type="label" data-view-component="true" className="sr-only position-absolute" aria-hidden="true" role="tooltip">network-me</tool-tip>
-                  </span>
-                  <span></span><span className="Label Label--secondary v-align-middle mt-1 no-wrap v-align-baseline Label--inline">Public</span>
-                </div>
-              </div>
-              <p className="pinned-item-desc color-fg-muted text-small mt-2 mb-0">
-                Starting framework for an auth-included social media application
-              </p>
-            </div>
-          </div>
-        </li>
-        
-        <li className="mb-3 d-flex flex-content-stretch sortable-button-item pinned-item-list-item js-pinned-item-list-item col-12 col-md-6 col-lg-6 reorderable">
-          <div className="Box d-flex p-3 width-full public source card">
-            <div className="pinned-item-list-item-content">
-              <div className="d-flex width-full position-relative">
-                <div className="flex-1">
-                  <svg aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true" className="octicon octicon-repo mr-1 color-fg-muted">
-                    <path d="M2 2.5A2.5 2.5 0 0 1 4.5 0h8.75a.75.75 0 0 1 .75.75v12.5a.75.75 0 0 1-.75.75h-2.5a.75.75 0 0 1 0-1.5h1.75v-2h-8a1 1 0 0 0-.714 1.7.75.75 0 1 1-1.072 1.05A2.495 2.495 0 0 1 2 11.5Zm10.5-1h-8a1 1 0 0 0-1 1v6.708A2.486 2.486 0 0 1 4.5 9h8ZM5 12.25a.25.25 0 0 1 .25-.25h3.5a.25.25 0 0 1 .25.25v3.25a.25.25 0 0 1-.4.2l-1.45-1.087a.249.249 0 0 0-.3 0L5.4 15.7a.25.25 0 0 1-.4-.2Z"></path>
-                  </svg>
-                  <span data-view-component="true" className="position-relative">
-                    <a data-hydro-click='{"event_type":"user_profile.click","payload":{"profile_user_id":129768978,"target":"PINNED_REPO","user_id":129768978,"originating_url":"https://github.com/sp12102001"}}' data-hydro-click-hmac="87ab20961ec4dad289769a689e3b29bad16e4d0611960311e38d90fca6aa8863" id="775401778" href="/sp12102001/python-game" data-view-component="true" className="Link mr-1 text-bold wb-break-word" aria-labelledby="tooltip-3dd11a9b-6eaf-4a33-9f70-0a5e1c525af2">
-                      <span className="repo">python-game</span>
-                    </a>
-                    <tool-tip id="tooltip-3dd11a9b-6eaf-4a33-9f70-0a5e1c525af2" for="775401778" popover="manual" data-direction="s" data-type="label" data-view-component="true" className="sr-only position-absolute" aria-hidden="true" role="tooltip">python-game</tool-tip>
-                  </span>
-                  <span></span><span className="Label Label--secondary v-align-middle mt-1 no-wrap v-align-baseline Label--inline">Public</span>
-                </div>
-              </div>
-              <p className="pinned-item-desc color-fg-muted text-small mt-2 mb-0">
-                A Python-based interactive maths game. Human plays against the computer in a test of speed and accuracy.
-              </p>
-            </div>
-          </div>
-        </li>
-        
-        <li className="mb-3 d-flex flex-content-stretch sortable-button-item pinned-item-list-item js-pinned-item-list-item col-12 col-md-6 col-lg-6 reorderable">
-          <div className="Box d-flex p-3 width-full public source card">
-            <div className="pinned-item-list-item-content">
-              <div className="d-flex width-full position-relative">
-                <div className="flex-1">
-                  <svg aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true" className="octicon octicon-repo mr-1 color-fg-muted">
-                    <path d="M2 2.5A2.5 2.5 0 0 1 4.5 0h8.75a.75.75 0 0 1 .75.75v12.5a.75.75 0 0 1-.75.75h-2.5a.75.75 0 0 1 0-1.5h1.75v-2h-8a1 1 0 0 0-.714 1.7.75.75 0 1 1-1.072 1.05A2.495 2.495 0 0 1 2 11.5Zm10.5-1h-8a1 1 0 0 0-1 1v6.708A2.486 2.486 0 0 1 4.5 9h8ZM5 12.25a.25.25 0 0 1 .25-.25h3.5a.25.25 0 0 1 .25.25v3.25a.25.25 0 0 1-.4.2l-1.45-1.087a.249.249 0 0 0-.3 0L5.4 15.7a.25.25 0 0 1-.4-.2Z"></path>
-                  </svg>
-                  <span data-view-component="true" className="position-relative">
-                    <a data-hydro-click='{"event_type":"user_profile.click","payload":{"profile_user_id":129768978,"target":"PINNED_REPO","user_id":129768978,"originating_url":"https://github.com/sp12102001"}}' data-hydro-click-hmac="87ab20961ec4dad289769a689e3b29bad16e4d0611960311e38d90fca6aa8863" id="725622582" href="/sp12102001/api-query-framework" data-view-component="true" className="Link mr-1 text-bold wb-break-word" aria-labelledby="tooltip-e36919c0-d29b-4efe-8fd5-cb24b73c330f">
-                      <span className="repo">api-query-framework</span>
-                    </a>
-                    <tool-tip id="tooltip-e36919c0-d29b-4efe-8fd5-cb24b73c330f" for="725622582" popover="manual" data-direction="s" data-type="label" data-view-component="true" className="sr-only position-absolute" aria-hidden="true" role="tooltip">api-query-framework</tool-tip>
-                  </span>
-                  <span></span><span className="Label Label--secondary v-align-middle mt-1 no-wrap v-align-baseline Label--inline">Public</span>
-                </div>
-              </div>
-              <p className="pinned-item-desc color-fg-muted text-small mt-2 mb-0">
-                Framework to make an API Query using Javascript on a Static Site using ChatGPT and Github Pages
-              </p>
-            </div>
-          </div>
-        </li>
-        
-      </ol>
+        ))}
+      </div>
     </section>
   );
 }
 
-export default projects;
+export default Projects;
