f6ec69fc50721947418aba257e5904f067a88eb1
SP <129768978+sp12102001@users.noreply.github.com>
2025-01-01 18:02:14 +0800

    Update About.jsx

--- src/About.jsx
+++ src/About.jsx
@@ -5,37 +5,80 @@ function About() {
   return (
     <section id="portfolio" className="container">
       <h2>About Me</h2>
-      <p>I am a highly motivated professional with a background in research, data analysis, and project management. My passion lies in harnessing emerging technologies to drive innovation and efficiency, and I bring diverse experience in developing and implementing technology-driven solutions both independently and collaboratively. I am eager to apply my skills and experiences to roles where I can continue to learn, develop, and contribute meaningfully.
-</p>
+      <p>
+        I am a highly motivated professional with a background in research, data analysis, as well as technical development and programming. 
+        My passion lies in harnessing emerging technologies to drive innovation and efficiency. I bring diverse 
+        experience in developing and implementing technology-driven solutions both independently and collaboratively. 
+      </p>
       <ul className="profile-links" aria-label="Profile links">
         <li>
-          <a href="https://www.cochrane.org/search/all/sanjana%20prabhakar" target="_blank" tabIndex="0">
-            <img src="https://www.cochrane.org/sites/default/files/public/uploads/join/badge_member.png" alt="Cochrane" />
+          <a 
+            href="https://www.cochrane.org/search/all/sanjana%20prabhakar" 
+            target="_blank" 
+            rel="noopener noreferrer" 
+            tabIndex="0"
+          >
+            <img 
+              src="https://www.cochrane.org/sites/default/files/public/uploads/join/badge_member.png" 
+              alt="Cochrane" 
+            />
           </a>
         </li>
         <li>
-          <a href="https://orcid.org/0000-0003-4840-7371" target="_blank" tabIndex="0">
-            <img src="https://orcid.org/sites/default/files/images/orcid_16x16.png" alt="ORCID" />
+          <a 
+            href="https://orcid.org/0000-0003-4840-7371" 
+            target="_blank" 
+            rel="noopener noreferrer" 
+            tabIndex="0"
+          >
+            <img 
+              src="https://orcid.org/sites/default/files/images/orcid_16x16.png" 
+              alt="ORCID" 
+            />
           </a>
         </li>
         <li>
-          <a href="https://www.researchgate.net/profile/S-Prabhakar" target="_blank" tabIndex="0">
-            <img src="https://theme.zdassets.com/theme_assets/11199769/02b935144b9a19c226cc526d6a8c452493d2bca1.png" alt="ResearchGate" />
+          <a 
+            href="https://www.researchgate.net/profile/S-Prabhakar" 
+            target="_blank" 
+            rel="noopener noreferrer" 
+            tabIndex="0"
+          >
+            <img 
+              src="https://theme.zdassets.com/theme_assets/11199769/02b935144b9a19c226cc526d6a8c452493d2bca1.png" 
+              alt="ResearchGate" 
+            />
           </a>
         </li>
         <li>
-          <a href="https://www.linkedin.com/in/sanjana-sp" target="_blank" tabIndex="0">
-            <img src="https://cdn-icons-png.flaticon.com/512/174/174857.png" alt="LinkedIn" />
+          <a 
+            href="https://www.linkedin.com/in/sanjana-sp" 
+            target="_blank" 
+            rel="noopener noreferrer" 
+            tabIndex="0"
+          >
+            <img 
+              src="https://cdn-icons-png.flaticon.com/512/174/174857.png" 
+              alt="LinkedIn" 
+            />
           </a>
         </li>
         <li>
-  <a href="https://sanjanap.tiiny.site/" target="_blank" tabIndex="0">
-    <img src="https://cdn-icons-png.flaticon.com/512/909/909212.png" alt="Download My Resume" />
-  </a>
-</li>
+          <a 
+            href="https://sanjanap.tiiny.site/" 
+            target="_blank" 
+            rel="noopener noreferrer" 
+            tabIndex="0"
+          >
+            <img 
+              src="https://cdn-icons-png.flaticon.com/512/909/909212.png" 
+              alt="Download My Resume" 
+            />
+          </a>
         </li>
       </ul>
     </section>
   );
 }
+
 export default About;
